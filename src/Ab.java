d3f51ebde7081daf4b3ea5e065598f3b97fff74d
henosis <henosis@henosis-Tower.(none)>
2013-02-28 05:16:39 +0000

    Node removed and ab modded

--- src/Ab.java
+++ src/Ab.java
@@ -12,10 +12,8 @@ public class Ab
 
 	public int search(State s)
 	{
-		//Create new node and call AbSearch for every child of the node with 
+		//Call AbSearch for every child of the state with 
 		//increasing depth size and return the best move (column number for best drop)
-
-		Node root = new Node(s, null);
 
 		//The bestMove the player can find for the state
 		int bestMove;
@@ -23,50 +21,43 @@ public class Ab
 		//The best result returned from search
 		int bestResult = Integer.MIN_VALUE;
 
-		//Get legal moves for the state in the node
-	    List<int> legalMoves = node.s.get_legal_moves();
+		//Get legal moves for the state
+	    List<int> legalMoves = s.get_legal_moves();
 
 	    for(move : legalMoves)
 	    {
-			int result = AbSearch(root.s.next_state(move), depth, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+			int result = AbSearch(s.next_state(move,true), depth, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
 			
 			//If the result for this child is greater than previous best result
 	    	//make bestMove the new result
 	    	if(result > bestResult)
-	    		bestMove = move;
+				bestMove = move;
 		}
 
 		return bestMove;
 	}
 
-	public int evaluation(Node node, boolean isWhite)
+	public int evaluation(State s, boolean isWhite)
 	{
 		return 1;
 	}
 
-	public int AbSearch(Node node, int depth, int a, int b, boolean isWhite)
+	public int AbSearch(State s, int depth, int a, int b, boolean isWhite)
 	{       
 		//Check if we sould go further down the tree TODO: PUT THE TIMER IN THE IF FUNCTION
-	    if(depth == 0 || node.s.isFinal())
-	        return evaluation(node, isWhite);
+	    if(depth == 0 || s.isFinal())
+	        return evaluation(s, isWhite);
 
-	    //Get legal moves for the state in the node
-	    List<int> legalMoves = node.s.get_legal_moves();
+	    //Get legal moves for the state
+	    List<int> legalMoves = s.get_legal_moves();
 
 	    //If player is the first player (WHITE-MAX)
 	    if(isWhite)
 	    {
-	    	//First get all the children of node from legal moves
-	    	List<Node> nodes = new ArrayList<int>();
-	    	for(move : legalMoves)
-	    	{
-	    		nodes.add(new Node( node.s.next_state(move, 1), node));
-	    	}
-
-	    	//Then for each child node calculate alpha value by callin AbSearch recursively
+	    	//For each child state calculate alpha value by callin AbSearch recursively
 	    	//and make alpha the largest value of the outcome
 	    	//Then to do the pruning check if a >= b and if so we can break out of the for loop
-	        for(child : nodes)
+	        for(child : s.next_state(move, true))
 	        {
 	            a = Math.max(a, AbSearch(child, depth-1, a, b, !isWhite));
 	            if(b =< a)
@@ -75,18 +66,15 @@ public class Ab
 
 	        return a;
 	    }
+
 	    //If player is the second player (RED-MIN)
 	    else
 	    {
-	    	//First get all the children of node from legal moves
-	    	List<Node> nodes = new ArrayList<int>();
-	    	for(move : legalMoves)
-	    	{
-	    		nodes.add(new Node( node.s.next_state(move, 0), node));
-	    	}
-
-
-	        for(child : nodes)
+	    	//For each child state calculate alpha value by callin AbSearch recursively
+	    	//and make alpha the largest value of the outcome
+	    	//Then to do the pruning check if a >= b and if so we can break out of the for loop
+	        
+	        for(child : s.next_state(move, false))
 	        {
 	            a = Math.min(b, AbSearch(child, depth-1, a, b, !isWhite));
 	            if(b =< a)
