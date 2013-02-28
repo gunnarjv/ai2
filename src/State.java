2c02632dee4e6ba4807d696d32b7f0ad78e9c400
gunnarjv <gunnarjv@gmail.com>
2013-02-28 00:36:03 +0000

    fixed trillion errors in state

--- src/State.java
+++ src/State.java
@@ -1,11 +1,16 @@
-import java.util.*
+import java.util.*;
 
 public class State
 {
-	
-
+	// LSB is bottom left on board. 
+	// Then comes the bottom left column.
+	// Followed by one unused bit.
+	// Followed by other columns.
 	long white = 0;
 	long red = 0;
+
+	public State()
+	{}
 
 	public State(int white, int red)
 	{
@@ -13,9 +18,21 @@ public class State
 		this.red = red;
 	}
 
-	public List<Int> get_legal_moves(String role)
+	public List<Integer> get_legal_moves()
 	{
-		return new List<Int>;
+		long board = white & red;
+		List<Integer> moves = new ArrayList<Integer>();
+		int shift = 7;
+
+		if((board & 1 << 5) != 0) moves.add(1);
+		if((board & 1 << shift+5) != 0) moves.add(2);
+		if((board & 1 << shift*2+5) != 0) moves.add(3);
+		if((board & 1 << shift*3+5) != 0) moves.add(4);
+		if((board & 1 << shift*4+5) != 0) moves.add(5);
+		if((board & 1 << shift*5+5) != 0) moves.add(6);
+		if((board & 1 << shift*6+5) != 0) moves.add(7);
+
+		return moves;
 	}
 
 	// Returns a state as if 'move' was made.
@@ -32,6 +49,16 @@ public class State
 		State that = (State) other;
 		if(this.white == that.white && this.red == that.red) return true;
 		return false;
+	}
+
+
+	int main()
+	{
+		State s = new State();
+
+		System.out.println(s.get_legal_moves());
+
+		return 0; 
 	}
 
 }
